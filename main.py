6028cbbdfcd467c130e14c05abb5b7bdf6a45de7
TaeKen Kim <67577780+tk0802kim@users.noreply.github.com>
2021-05-05 02:35:30 +0900

    5/5/update

--- main.py
+++ main.py
@@ -19,15 +19,11 @@ from nordvpn_switcher import initialize_VPN,rotate_VPN
 from selenium import webdriver
 import re
 import webbrowser
+from datetime import datetime
+
+
 location = ["New York, NY"]
 site = 'indeed'
-
-
-
-
-
-
-
 
 
 
@@ -49,8 +45,16 @@ driver = webdriver.Chrome()
 
 #load pickle
 if 'indeed' in site:
-    with open("indeed_listing.pkl","rb") as f:
-        df_indeed = pickle.load(f)
+    try:
+        with open("indeed_listing.pkl","rb") as f:
+            df_indeed = pickle.load(f)
+    except:
+        print('Making new joblists')
+
+#%%
+#additional fields
+custom_fields=["applied","text","tags","date_collected",'Order','Outcome','date_applied']
+
 
 #variables used
 page_i = np.zeros(len(location),dtype=int)
@@ -76,67 +80,137 @@ for i_loc, loc in enumerate(location):
             if "df_indeed" in locals():
                 df_id = pd.DataFrame(jobmap.values())
                 df_id.set_index('jk',inplace=True)
-                
                 df_list = [df_id.loc[row,:] for row in df_id.index if row not in df_indeed.index]
-                
                 df_indeed = df_indeed.append(df_list)                            
                 
             else:
                 df_indeed = pd.DataFrame(jobmap.values())
-                df_indeed["applied"]=''
-                df_indeed["text"]=''
                 df_indeed.set_index('jk',inplace=True)
                 
+            for field in custom_fields:
+                if field not in df_indeed.columns:
+                    df_indeed[field] = ''
+                    
+                    
+                # df_indeed["applied"]=''
+                # df_indeed["text"]=''
+                
             page_i += 50
-            if page_i > 300:
+            if page_i > 200:
                 break
             sleep(1)
-            
-            
+#%%
+"""order maker and nan remover"""
+
+counter = int(df_indeed['Order'].max())+1
+for jk, row in df_indeed.iterrows():
+    if np.isnan(row['Order']):
+        df_indeed.loc[jk,'Order'] = counter
+        counter+=1
+    
+    # for col in custom_fields:
+    #     try:
+    #         if np.isnan(row[col]):
+    #             df_indeed.loc[jk,col] = ''
+    #     except:
+    #         continue
+
+df_indeed.fillna('',inplace=True)
+with open("indeed_listing.pkl","wb") as f:
+    pickle.dump(df_indeed,f)
+#%%
+"""description getter"""
+
 for jk, row in tqdm(df_indeed.iterrows()):
     url = "https://www.indeed.com/viewjob?jk="+jk
     
-    html = requests.get(url,headers=headers)
-    soup = BeautifulSoup(html.text, 'html.parser')
+    if len(df_indeed['text'][jk])==0:
     
-    while 'Captcha' in soup.title.text:
-        #webbrowser.open('url', new=0)
-        rotate_VPN(instructions)
-        html = requests.get(url)
-        soup = BeautifulSoup(html.text, 'html.parser')  
+        html = requests.get(url,headers=headers)
+        soup = BeautifulSoup(html.text, 'html.parser')
         
-    result= soup.find('div', {'id':'jobDescriptionText'})
-    df_indeed.loc[jk,'text']= result.text
+        while 'Captcha' in soup.title.text:
+            #webbrowser.open('url', new=0)
+            rotate_VPN(instructions)
+            html = requests.get(url)
+            soup = BeautifulSoup(html.text, 'html.parser')  
+            
+        result= soup.find('div', {'id':'jobDescriptionText'})
+        df_indeed.loc[jk,'text']= result.text
     
-    #get phd stuff
-    phd_list = ['phd','ph.d','ph d','ph. d']
-    for phd in phd_list:
-        if re.search(phd, result.text, re.IGNORECASE):
-            df_indeed.loc[jk,'applied'] = 'phd'
+    
+    if len(df_indeed['date_collected'][jk])==0:
+        df_indeed.loc[jk,'date_collected']=datetime.now().strftime("%m/%d/%Y")
     
 with open("indeed_listing.pkl","wb") as f:
     pickle.dump(df_indeed,f)
 
 
 
+"""tagger"""
+for jk, row in df_indeed.iterrows():
+    if len(df_indeed['text'][jk])!=0:
+        #get phd stuff
+        phd_list = ['phd','ph.d','ph d','ph. d']
+        for phd in phd_list:
+            if re.search(phd, result.text, re.IGNORECASE):
+                # if type(df_indeed['tags'][jk])!=str:
+                #     df_indeed.loc[jk,'tags'] = 'phd'
+                # elif not('phd' in df_indeed.loc[jk,'tags']):
+                #     df_indeed.loc[jk,'tags'] = df_indeed.loc[jk,'tags']+', phd'
+                if len(df_indeed['tags'][jk])==0:
+                    df_indeed.loc[jk,'tags'] = 'phd'
+                elif not('phd' in df_indeed.loc[jk,'tags']):
+                    df_indeed.loc[jk,'tags'] = df_indeed.loc[jk,'tags']+', phd'
+                    
 
 
 
-
-
-def parse_phd():
-    for jk, row in df_indeed.iterrows():
-        url = "https://www.indeed.com/viewjob?jk="+jk
+#%%        
         
-        if (row['applied'] =='phd') and (row['applied']!='applied') and (row['applied']!='not'):
+def parse_tags(*tags):
+    #showlater
+    for tag in tags:
+        for i_ord in reversed(range(df_indeed.shape[0])):   
             
-           webbrowser.open(url, new=0) 
-           donext = input("What's next? Type 'applied', 'not', or 'stop'")
-           
-           if donext in ['applied','not']:
-               df_indeed.loc[jk,'applied']=donext
-           elif donext == 'stop':
-               break
-              
-    with open("indeed_listing.pkl","wb") as f:
-        pickle.dump(df_indeed,f)
+            jk = df_indeed.index[df_indeed['Order']==i_ord][0]
+            url = "https://www.indeed.com/viewjob?jk="+jk    
+            
+            show_entry=False
+            if (tag in df_indeed.loc[jk,'tags']) :
+                if len(df_indeed.loc[jk,'applied'])==0:
+                    show_entry = True
+                elif (df_indeed.loc[jk,'applied']!='not') and not(df_indeed.loc[jk,'applied'][0].isnumeric()):
+                    show_entry = True
+                
+                if show_entry:
+                    webbrowser.open(url, new=0) 
+                    donext = input("What's next? Type 'applied', 'not', 'later', or 'stop':   \n")
+                   
+                    if donext in ['applied','not','later']:
+                        df_indeed.loc[jk,'applied']=donext
+                        if donext == 'applied':
+                            df_indeed.loc[jk,'applied'] = datetime.now().strftime("%m/%d/%Y")
+                            
+                    elif donext == 'stop':
+                        break
+
+        else:
+            continue
+                  
+        with open("indeed_listing.pkl","wb") as f:
+            pickle.dump(df_indeed,f)
+            
+        break
+
+def update_status(company):
+    hits = df_indeed.loc[(df_indeed['cmp']==company)&[len(entry)>6 for entry in df_indeed['applied']],:]
+    for n, title in enumerate(hits['title']):
+        print('{}: {}'.format(n+1,title))
+    print(' ')
+    i_job = input("Which one? \n")
+    print(' ')
+    result = input("Update Result \n")
+    
+    jk = hits.index[int(i_job)-1]
+    df_indeed.loc[jk,'Outcome'] +='\n'+ result+' - '+datetime.now().strftime("%m/%d/%Y")
